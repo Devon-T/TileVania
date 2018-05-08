94bc4018a2427de3d02e080b85632d22c1ede62e
LAPTOP-DEVON-20\dtoland <devon.toland@live.com>
2018-05-08 07:56:27 -0500

    run and jump motion script, added running aimator transition to code, 2D physics update to create better responsive fall/jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerController : MonoBehaviour {
+
+    //config
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpStrength = 8f;
+
+    //Statw
+    bool isAlive = true;
+
+    //Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    //Message the methods  
+    void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public void OnPlayerDeath()
+    {
+        isAlive = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isAlive) { return; }
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpStrength);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void Run()
+    {
+        
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(runSpeed * controlThrow, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+
+     }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            //use sign to get a value of +1 or -1 to get the local scale
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+}
