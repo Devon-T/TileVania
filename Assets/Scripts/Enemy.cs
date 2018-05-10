a6465dd8a0c127b34da9d7266f9d092e2545ebdc
LAPTOP-DEVON-20\dtoland <devon.toland@live.com>
2018-05-10 09:53:12 -0500

    Gave enemy movement and ability to turn around when hits a wall or end of a cliff

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D myRigidBody;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+
 	// Use this for initialization
 	void Start () {
-		
+        myRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Walk();
 	}
+
+    private void Walk()
+    {
+        // if enemy collides with a wall on body collider or do not collide with ground with feet collider then moveSpeed * -1
+        if (IsFacingRight())
+        {
+
+            myRigidBody.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-moveSpeed, 0);
+        }
+
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -130,4 +130,6 @@ public class PlayerController : MonoBehaviour {
         }
 
     }
+
+    private void 
 }
