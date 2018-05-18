2c8a0ff3a3ed3fa3038badbca07f84f1030db6b1
LAPTOP-DEVON-20\dtoland <devon.toland@live.com>
2018-05-18 15:14:53 -0500

    Removed duplicated scenes made for shared screen layout due to UI issues in development. Created Menu and two levels, created level load trigger, created Basic Menud Canvas and Start Game button script. Placed Controls button in place but still need to build controls screen to let user set custom controls or at least view current controls then set button script to load scene.

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float lvlExitSlowMoFactor = 0.2f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("touched next level trigger");
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel ()
+    {
+        print("called coroutine");
+        Time.timeScale = lvlExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        print("passed yield statement");
+        Time.timeScale = 1f;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
+
